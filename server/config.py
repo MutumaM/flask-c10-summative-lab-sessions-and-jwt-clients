d3c854509d769d44c8322de2e2b152793870d8de
MutumaM <jaydenmwongera@gmail.com>
2026-07-31 21:29:02 +0300

    config.py-corrected

--- server/config.py
+++ server/config.py
@@ -5,14 +5,14 @@ from flask_bcrypt import Bcrypt
 from flask_restful import Api
 
 app = Flask(__name__)
-app.secret_key = b'\xf3\x9a\x1c\xd4\x8e\x02\x91\xbb'  # used to encrypt session cookies
+app.secret_key = b'\xf3\x9a\x1c\xd4\x8e\x02\x91\xbb'  
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 db = SQLAlchemy()
-migrate = Migrate(app, db)
-db.init_app(app)
+db.init_app(app)              
 
+migrate = Migrate(app, db)     
 bcrypt = Bcrypt(app)
 api = Api(app)
